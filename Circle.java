10f46fc53d92cac2907170d117aa3cf8cf273933
Yazaaan <ysawalha05@gmail.com>
2026-02-14 00:07:41 +0100

    Es werde Kreis

--- Circle.java
+++ Circle.java
@@ -7,28 +7,27 @@ import java.awt.BasicStroke;
 // Die Klasse die einen Kreis beschreibt
 // Orientiert sich an der Funktion der Datenstruktur der doppelt verketteten Liste
 
-public class Circle {
+public class Circle extends DrawableObject{
     private Circle previous, next;
     private int x, y, r;
     private boolean activated;
-    Graphics g;
     Color color_active = new Color(255, 203, 0);
     Color color_inactive = new Color(127, 101, 0);
-    private BasicStroke stroke = new BasicStroke(
+    private BasicStroke stroke_dashed = new BasicStroke(
             2.0f,
             BasicStroke.CAP_BUTT,
             BasicStroke.JOIN_MITER,
             10.0f, new float[]{10.0f}, 0.0f
         );
+    private BasicStroke stroke_solid = new BasicStroke(20.0f);
 
-    public Circle(int x, int y, int r, Graphics g){
+    public Circle(int x, int y, int r){
         previous = this;
         next = this;
         this.x = x;
         this.y = y;
         this.r = r;
         activated = false;
-        this.g = g;
     }
 
     public void stateSwitch(){
@@ -37,11 +36,14 @@ public class Circle {
         next.activated = !next.activated;   // Zustand des nachfolgenden Knoten ändern
     }
 
-    private void paint(){
+    public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(activated? color_active : color_inactive);
-        g2d.setStroke(stroke);
+        g2d.setStroke(stroke_solid);
+        g2d.setColor(Color.BLACK);
         g2d.drawOval(x-r, y-r, 2*r, 2*r);
+        g2d.setColor(activated? color_active : color_inactive);
+        g2d.fillOval(x-r, y-r, 2*r, 2*r);
+        
     }
 
     public void insert(Circle newCircle){
--- GamePanel.java
+++ GamePanel.java
@@ -11,11 +11,26 @@ import javax.swing.JLabel;
 
 public class GamePanel extends JPanel{
     private GameFrame frame;
-    
+    DrawableObject[] objs = new DrawableObject[100];
+
     public GamePanel(GameFrame frame)
     {
         this.frame = frame;
 
         this.add(new JLabel("Spielfeld"));
+
+        Circle rootCircle = new Circle(100, 100, 50);
+        objs[0] = rootCircle;
+        
+        //repaint();
+
+        
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        for(DrawableObject o : objs) if(o != null) o.paint(g);
+
     }
 }
