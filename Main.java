516273afbfc01d8d748a2dd7687e687eea715113
Yazaaan <ysawalha05@gmail.com>
2026-02-16 22:02:18 +0100

    GitHub hinterlassen

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-// Einstieg in das Programm ist hier
+// GitHub-Repo zu diesem Projekt: https://github.com/Yazaaan/oop_wise2526
 
+// Einstieg in das Programm ist hier
 public class Main{
     public static void main(String[] args){
-        Launcher GameLauncher = new Launcher(4); //test
+        Launcher GameLauncher = new Launcher(4);
     }
 }
