578bd20ffb333b49ff5a460deef348751d002c8e
Yazaaan <ysawalha05@gmail.com>
2026-02-16 19:57:57 +0100

    Launcher Code übersichtlicher gemacht

--- Launcher.java
+++ Launcher.java
@@ -25,44 +25,43 @@ public class Launcher extends JFrame{
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        
+
         // Zahleneingabe mit JSpinner
         SpinnerModel spinnerModel = new SpinnerNumberModel(5, 4, 20, 1);    // SpinnerModel: Startwert, Minimum, Maximum, Schrittweite
         JSpinner num_difficulty = new JSpinner(spinnerModel);
         num_difficulty.setValue(new Integer(defaultValue));
-        
+
         // "Neues Spiel" Button
         JButton btn_newGame = new JButton("Neues Spiel starten");
-        btn_newGame.addActionListener(e -> startNewGame((Integer)num_difficulty.getValue()));
-        
+        btn_newGame.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("Neues Spiel mit " + num_difficulty.getValue() + " Schaltern wird gestartet...");
+
+                    GameFrame game = new GameFrame((int)num_difficulty.getValue());
+
+                    dispose();
+                }
+            });
+
         // Beenden Button
         JButton btn_quit = new JButton("Beenden");
         btn_quit.addActionListener(new ActionListener(){
-           public void actionPerformed(ActionEvent e){
-               System.exit(0);
-           }
-        });
-        
+                public void actionPerformed(ActionEvent e){
+                    System.exit(0);
+                }
+            });
+
         // Panel beschreiben und Elemente einfügen
         JPanel panel_root = new JPanel();
         panel_root.setLayout(new GridLayout(2, 2, 20, 20));
         panel_root.setBorder(BorderFactory.createEmptyBorder(40, 20, 40, 20)); // 20px Padding an allen Seiten (oben, links, unten, rechts)
-        
+
         panel_root.add(new JLabel("Anzahl Schalter"));
         panel_root.add(num_difficulty);
         panel_root.add(btn_quit);
         panel_root.add(btn_newGame);
-        
-        
+
         this.add(panel_root);
         setVisible(true);
-    }
-    
-    private void startNewGame(int diffi){
-        System.out.println("Neues Spiel mit " + diffi + " Schaltern wird gestartet...");
-        
-        GameFrame game = new GameFrame(diffi);
-        
-        dispose();
     }
 }
